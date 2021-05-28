44db561e0a562e66bca8f7c2ec6cee6231079e4e
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-28 13:09:23 -0500

    fetch post request in new meet up comp

--- src/components/MeetUps/NewMeetupForm.js
+++ src/components/MeetUps/NewMeetupForm.js
@@ -4,11 +4,27 @@ import styles from "./NewMeetUpForm.module.css";
 
 const NewMeetUpForm = (props) => {
   const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
 
   const submitHandler = (e) => {
     e.preventDefault();
 
     const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredaddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetUpData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredaddress,
+      description: enteredDescription,
+    };
+
+    console.log(meetUpData);
+    props.onAddMeetUp(meetUpData);
   };
 
   return (
@@ -20,15 +36,21 @@ const NewMeetUpForm = (props) => {
         </div>
         <div className={styles.control}>
           <label htmlFor="image">Meet Up Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputRef} />
         </div>
         <div className={styles.control}>
           <label htmlFor="address">Meet Up Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputRef} />
         </div>
         <div className={styles.control}>
           <label htmlFor="description">Meet Up Description</label>
-          <textarea type="text" required id="description" rows="5" />
+          <textarea
+            type="text"
+            required
+            id="description"
+            rows="5"
+            ref={descriptionInputRef}
+          />
         </div>
         <div className={styles.actions}>
           <button>Add Meet Up</button>
--- src/pages/NewMeetUp.js
+++ src/pages/NewMeetUp.js
@@ -1,10 +1,20 @@
 import NewMeetUpForm from "../components/MeetUps/NewMeetupForm";
 
 const NewMeetUpsPage = () => {
+  const addMeetUpHandler = (meetUpData) => {
+    fetch("someURLhere...", {
+      method: "POST",
+      body: JSON.stringify(meetUpData),
+      headers: {
+        "Content-Type": "applicaiton/json",
+      },
+    });
+  };
+
   return (
     <section>
       <h1>Add New Meet Up</h1>
-      <NewMeetUpForm />
+      <NewMeetUpForm onAddMeetUp={addMeetUpHandler} />
     </section>
   );
 };
