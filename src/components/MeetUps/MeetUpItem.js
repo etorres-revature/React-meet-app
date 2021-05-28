e98556df6aeab357ebfa258fabbcd750af6afb89
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-28 12:38:17 -0500

    UI card comp with children props

--- src/components/MeetUps/MeetUpItem.js
+++ src/components/MeetUps/MeetUpItem.js
@@ -1,20 +1,23 @@
+import Card from "../UI/Card";
 import styles from "./MeetUpItem.module.css";
 
 const MeetUpItem = (props) => {
   return (
-    <li className={styles.item}>
-      <div className={styles.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={styles.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={styles.actions}>
-        <button>To Favorites</button>
-      </div>
-    </li>
+    <Card>
+      <li className={styles.item}>
+        <div className={styles.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={styles.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={styles.actions}>
+          <button>To Favorites</button>
+        </div>
+      </li>
+    </Card>
   );
 };
 
