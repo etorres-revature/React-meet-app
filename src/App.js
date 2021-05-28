c07d79c7771c98a7ff5f64b28e109ea31089c776
Eric D. Torres <eric.d.torres@ibm.com>
2021-05-28 11:58:37 -0500

    router added to index and routes added to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route } from "react-router-dom";
+
+import AllMeetUpsPage from "./pages/AllMeetUps";
+import NewMeetUpsPage from "./pages/NewMeetUp";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Route exact path="/">
+        <AllMeetUpsPage />
+      </Route>
+      <Route exact path="/new-meetup">
+        <NewMeetUpsPage />
+      </Route>
+      <Route exact path="/favorites">
+        <FavoritesPage />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
